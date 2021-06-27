82ca583e7251fd5654805e3e8b0cc30e17c96872
Paul Moore <pdmoore@gmail.com>
2021-06-27 15:06:25 -0700

    recreate nunit 3 examples based on 'nunit3-example' repo

---
+++ nunit_3.12/NUnit3_Tests/ExampleTest.cs
@@ -0,0 +1,51 @@
+using System;
+using SystemUnderTest;
+using NUnit.Framework;
+
+namespace NUnit3_Tests
+{
+    public class ExampleTest
+    {
+        [Test]
+        public void Number_NotDivisibleBy_3or5_ReturnsNumber() {
+            Assert.AreEqual("1", Fizzbuzz.Of(1));
+        }
+        
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        [TestCase(99)]
+        public void Number_DivisibleBy3_ReturnsFizz(int value) {
+            Assert.AreEqual("Fizz", Fizzbuzz.Of(value));
+        }
+
+        [Test]
+        public void Number_Beyond_100_NotSupported() {
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                () => Fizzbuzz.Of(101));
+            Assert.AreEqual("Number must be within 1 to 100", ex.Message);
+        }
+
+        [Test]
+        public void Alternative_Style_Expecting_Exception() {
+            Assert.That(() => Fizzbuzz.Of(101), 
+                Throws.ArgumentException.With.Message.EqualTo("Number must be within 1 to 100"));
+        }
+
+        [Test]
+        [Ignore("Ask PO how to correctly handle this case")]
+        public void Negative_Numbers() {
+            Assert.Fail("unknown behavior");
+        }
+
+        [Test]
+        public void Verify_Trainer_ContactInfo() {
+            Contacts.Info actual = Contacts.get("Paul");
+            Assert.Multiple(() => {
+                Assert.AreEqual("Paul", actual.FirstName);
+                Assert.AreEqual("Moore", actual.LastName);
+                Assert.AreEqual("@pdmoore", actual.TwitterHandle);
+            });
+        }
+    }
+}
---
+++ nunit_3.12/SystemUnderTest/Fizzbuzz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SystemUnderTest {
+    public static class Fizzbuzz {
+        public static string Of(int number) {
+            if (number > 100) {
+                throw new ArgumentException("Number must be within 1 to 100");
+            }
+            if (number % 3 == 0) return "Fizz";
+            return "1";
+        }
+    }
+}
