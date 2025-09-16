cae542b83c75066a0e9baad97127840881422cae
Paul Moore <pdmoore@gmail.com>
2025-09-16 16:51:23 -0700

    java - add leap age example for assertions section

---
+++ junit_5.6.2/src/main/java/com/rocketnine/junitexamples/Hiker.java
@@ -0,0 +1,12 @@
+package com.rocketnine.junitexamples;
+
+import java.time.LocalDate;
+
+public class Hiker {
+
+    // stubbed implementation
+    // only test code is used in slide deck
+    public static int calculateLeapAge(LocalDate birthDate, LocalDate currentDate) {
+        return 0;
+    }
+}
---
+++ junit_5.6.2/src/test/java/com/rocketnine/junitexamples/LeapAgeTests.java
@@ -0,0 +1,22 @@
+package com.rocketnine.junitexamples;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class LeapAgeTests {
+
+    @Test
+    void someone_born_after_most_recent_leap_year_has_leap_age_0() {
+        // Arrange
+        LocalDate birthDate = LocalDate.parse("2025-07-04");
+        LocalDate currentDate = LocalDate.parse("2025-09-01");
+
+        // Act
+        int actual = Hiker.calculateLeapAge(birthDate, currentDate);
+
+        // Assert
+        Assertions.assertEquals(0, actual);
+    }
+}
