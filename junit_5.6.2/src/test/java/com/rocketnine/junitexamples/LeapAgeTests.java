79b747bb8d0bba173eedcc9ba4fd1b60fae1ce69
Paul Moore <pdmoore@gmail.com>
2025-09-17 10:42:05 -0700

    java - use github username in multi-assert example

--- junit_5.6.2/src/test/java/com/rocketnine/junitexamples/LeapAgeTests.java
+++ junit_5.6.2/src/test/java/com/rocketnine/junitexamples/LeapAgeTests.java
@@ -19,4 +19,11 @@ class LeapAgeTests {
         // Assert
         Assertions.assertEquals(0, actual);
     }
+
+    @Test
+    void someone_born_after_most_recent_leap_year_has_leap_age_0_CONDENSED() {
+        // Arrange - Act - Assert all in one line
+        Assertions.assertEquals(0, Hiker.calculateLeapAge(LocalDate.parse("2025-07-04"), LocalDate.parse("2025-09-01")));
+    }
+
 }
