f4d681f0c604caac12787705426c0fa5dd1ee6ff
Paul Moore <pdmoore@gmail.com>
2025-09-17 09:16:51 -0700

    c#/nunit - provide Format for the date string parser

--- nunit_3.12/NUnit3_Tests/LeapAgeTests.cs
+++ nunit_3.12/NUnit3_Tests/LeapAgeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using NUnit.Framework;
 
 namespace NUnit3_Tests
@@ -10,8 +11,8 @@ namespace NUnit3_Tests
         public void Someone_born_after_most_recent_leap_year_has_leap_age_0()
         {
             // Arrange
-            DateTime birthDate   = DateTime.Parse("2025-07-04");
-            DateTime currentDate =  DateTime.Parse("2025-09-16");
+            var birthDate   = DateTime.Parse("2025-07-04", new CultureInfo("en-US"));
+            var currentDate =  DateTime.Parse("2025-09-16", new CultureInfo("en-US"));
 
             // Act 
             var actual = Hiker.CalculateLeapAge(birthDate, currentDate);
