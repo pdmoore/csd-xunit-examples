eadfd89ba83ab28cce039d7d72c93035faf8cc23
Paul Moore <pdmoore@gmail.com>
2025-09-18 16:57:28 -0700

    python/unittest - add example for leap age

--- python/test_leap_age_pytest.py
+++ python/test_leap_age_pytest.py
@@ -1,15 +1,16 @@
+import unittest
+
+
+
 class Hiker:
     def calculate_leap_age(self, birth_date, current_date):
         return 0
 
 
-
-
-
-
+# pytest style assert
 def test_someone_born_after_most_recent_leap_year_has_leap_age_0():
     # Arrange
-    birth_date   = "2025-07-04"
+    birth_date = "2025-07-04"
     current_date = "2025-09-01"
 
     # Act
@@ -17,3 +18,20 @@ def test_someone_born_after_most_recent_leap_year_has_leap_age_0():
 
     # Assert
     assert actual == 0
+
+
+
+
+
+# unittest style assert
+class LeapAgeTests(unittest.TestCase):
+    def test_someone_born_after_most_recent_leap_year_has_leap_age_0(self):
+        # Arrange
+        birth_date = "2025-07-04"
+        current_date = "2025-09-01"
+
+        # Act
+        actual = Hiker().calculate_leap_age(birth_date, current_date)
+
+        # Assert
+        self.assertEqual(0, actual)
