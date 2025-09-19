20abd08cc92d3da9d355883d4dbd10957feda51b
Paul Moore <pdmoore@gmail.com>
2025-09-19 12:25:28 -0700

    javascript/jest - explicit require for jest elements

--- jest/test/LeapAge.test.js
+++ jest/test/LeapAge.test.js
@@ -1,20 +1,23 @@
+const { describe, it, expect } = require('@jest/globals')
+
 function calculateLeapAge(birth_date, current_date) {
     return 0;
 }
 
 
+describe('LeapAge examples', () => {
+
+    it('someone born after most recent leap year has a leap age of 0', () => {
+        // Arrange
+        const birth_date = new Date("2025-07-04");
+        const current_date = new Date("2025-09-01");
+
+        // Act
+        let actual = calculateLeapAge(birth_date, current_date);
+
+        // Assert
+        expect(actual).toBe(0);
+    });
+})
 
 
-
-
-it('someone born after most recent leap year has a leap age of 0', () => {
-    // Arrange
-    var birth_date   = new Date("2025-07-04");
-    var current_date = new Date("2025-09-01");
-
-    // Act
-    var actual = calculateLeapAge(birth_date, current_date);
-
-    // Assert
-    expect(actual).toBe(0);
-});
